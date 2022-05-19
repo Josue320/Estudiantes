6e2917aaecb8d56df66ff14cf4e97aff379a57cc
josue320 <josue.flores68u@std.uni.edu.ni>
2022-05-19 19:23:17 +0200

    Estructura limpia

---
+++ AppCore/Services/EstudianteService.cs
@@ -0,0 +1,40 @@
+﻿using AppCore.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Services
+{
+    public class EstudianteService : IEstudianteService
+    {
+        private IEstudianteRepository estudianteRepository;
+        public EstudianteService(IEstudianteRepository estudianteRepository)
+        {
+            this.estudianteRepository = estudianteRepository;
+        }
+
+        public void Create(Estudiante t)
+        {
+            estudianteRepository.Create(t);
+        }
+
+        public bool Delete(Estudiante t)
+        {
+            return estudianteRepository.Delete(t);
+        }
+
+        public List<Estudiante> GetAll()
+        {
+            return estudianteRepository.GetAll();
+        }
+
+        public int Update(Estudiante t)
+        {
+            return estudianteRepository.Update(t);
+        }
+    }
+}
---
+++ Infraestructura/Repository/EstudianteRepository.cs
@@ -0,0 +1,40 @@
+﻿using Domain.DepreciationDBEntities;
+using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructura.Repository
+{
+    public class EstudianteRepository : IEstudianteRepository
+    {
+        public IPepitoSchoolContext context;
+        public EstudianteRepository(IPepitoSchoolContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Estudiante> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
